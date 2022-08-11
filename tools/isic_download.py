c52a2983107a47a24d71e231b31f152e8bee6131
Anton Serhiiovych Myshenin <anton.myshenin@codete.com>
2022-08-11 18:56:14 +0200

    isic download

--- tools/isic_download.py
+++ tools/isic_download.py
@@ -1,8 +1,9 @@
-import click
+import os
+import requests
 import pandas as pd
 
 BASE_URL = 'https://api.isic-archive.com/api/v2'
-INTERNAL_LIMIT = 1000
+PORTION_SIZE = 1000
 
 
 def flatten_dict(d, sep='.'):
@@ -10,10 +11,26 @@ def flatten_dict(d, sep='.'):
     return flat_dict
 
 
-@click.command()
-@click.option('--limit', default=1000, type=click.INT)
-@click.option('--offset', default=1000, type=click.INT)
-@click.option('--csv', default='data.csv', type=click.STRING)
-@click.option('--directory', default='isic', type=click.STRING)
-def main(limit, offset, csv, directory):
-    pass
+def main():
+    portion = requests.get(f'{BASE_URL}/images?limit={PORTION_SIZE}&offset=0').json()
+    rows = []
+
+    while portion.get('next'):
+        for r in portion.get('results'):
+            rows.append(pd.DataFrame(flatten_dict(r), index=[0]))
+
+            download_request = requests.get(f"{r.get('files').get('full').get('url')}")
+            download_request.raise_for_status()
+            image_path = os.path.join(f'images', f"{r['isic_id']}.jpg")
+            with open(image_path, 'wb') as output_stream:
+                for chunk in download_request:
+                    output_stream.write(chunk)
+
+        portion = requests.get(portion.get('next')).json()
+
+    isic_table = pd.concat(rows)
+    isic_table.to_csv('isic_table.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
